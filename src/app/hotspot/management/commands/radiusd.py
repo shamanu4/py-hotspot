cba31b1e82defaca0a12f89c44ba023688f7fd5d
= <shamanu4@gmail.com>
2014-08-06 10:58:22 +0300

    management command

---
+++ src/app/hotspot/management/commands/radiusd.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'maxim'
+
+from radius import  AuthServer
+from pyrad import dictionary
+from pyrad import server
+from django.core.management.base import BaseCommand, CommandError
+from time import sleep
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        srv = AuthServer(dict = dictionary.Dictionary( "app/radius/dicts/dictionary" ))
+        srv.hosts['192.168.39.211'] =  server.RemoteHost('192.168.39.211','hotsp1','hotsp1.it-tim.net')
+        srv.hosts['192.168.70.211'] =  server.RemoteHost('192.168.70.211','hotsp2','hotsp2.it-tim.net')
+        srv.hosts['10.11.8.30'] =  server.RemoteHost('10.11.8.30','hotsp3','hotsp3.it-tim.net')
+        srv.hosts['192.168.33.33'] =  server.RemoteHost('192.168.33.33','thedude','thedude.it-tim.net')
+        srv.hosts['192.168.33.152'] =  server.RemoteHost('192.168.33.152','dev','dev.it-tim.net')
+        srv.BindToAddress('192.168.33.70')
+        sleep(5)
+        srv.Run()
